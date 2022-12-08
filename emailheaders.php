e83ce526e5e720236d3e788ce276609946adb48f
Sampath <kavilashtech@gmail.com>
2022-12-08 19:30:24 +0530

    initial commit

---
+++ emailheaders.php
@@ -0,0 +1,15 @@
+<?php 
+
+ini_set("mail.log", "tmp/mail.log");
+ini_set("mail.add_x_header", TRUE);
+
+
+ ini_set('mail.add_x_header','On'); 
+
+ ini_set('sendmail_from','icreach2022@gmail.com'); 
+ 
+ ini_set('SMTP','smtp.gmail.com'); 
+ 
+ ini_set('smtp_port','465'); 
+ 
+ ?>
